48bde91a1eeccd16200b836583af12df7388fa25
Zachary Casares <rrumble250@gmail.com>
2022-10-02 17:19:57 -0700

    added styling to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
   resources :sessions, only: [:new, :index]
-  get "signup", to: 'sessions#new'
+  get "login", to: 'sessions#new'
   root 'chatroom#index'
 end
