47eecf12ef1f2deb84f2db118b85ff31afe1b280
Zachary Casares <rrumble250@gmail.com>
2022-10-03 17:52:58 -0700

    add messages to index

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,6 +2,7 @@ class ChatroomController < ApplicationController
     before_action :require_user 
     def index
         @messages = Message.all
+        @message = Message.new
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  
   resources :sessions, only: [:new, :index]
   get "login", to: 'sessions#new'
   root 'chatroom#index'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 
 end
